2422d1789f7081bc3bc72d148ceba2dcd7f7c249
Leon Gregori <mail@leongregori.com>
2020-10-13 07:13:11 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,11 +13,9 @@ resource "aws_s3_bucket" "terraform-state-storage-s3" {
     versioning {
       enabled = true
     }
- 
     lifecycle {
       prevent_destroy = true
     }
- 
     tags = {
       Name = "S3 Remote Terraform State Store"
       Environment = "dev"
